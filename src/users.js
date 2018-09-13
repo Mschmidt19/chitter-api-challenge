d0f4955e7de77af49f4ebc89c0f3a17a7d922367
Marek <MarekSchmidt@outlook.com>
2018-09-13 16:54:33 +0100

    Sign in creates new session, need to connect signup to cause a signin after and a redirect to peeps page on both

--- src/users.js
+++ src/users.js
@@ -2,7 +2,8 @@
   'use strict'
 
   function Users() {
-
+    this.user_id;
+    this.session_key;
   }
 
   Users.prototype.newUser = function(handle, password, confirmPassword) {
@@ -25,6 +26,27 @@
     .then((response) => response.json())
     .then((data) => console.log(data))
   }
+  };
+
+  Users.prototype.logIn = function(handle, password) {
+    fetch('https://chitter-backend-api.herokuapp.com/sessions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        session: {
+          handle: handle,
+          password: password
+        }
+      })
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data)
+      this.user_id = data["user_id"];
+      this.session_key = data["session_key"];
+    })
   };
 
   exports.Users = Users;
